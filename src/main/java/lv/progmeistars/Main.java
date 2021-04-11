407adf855bd160d63831f5903aa646078f806c0c
dserveroriginal <60070115+dserveroriginal@users.noreply.github.com>
2021-04-11 17:50:34 +0300

    Added menu & working score output

--- src/main/java/lv/progmeistars/Main.java
+++ src/main/java/lv/progmeistars/Main.java
@@ -1,10 +1,10 @@
 package lv.progmeistars;
 
-import lombok.var;
 import lv.progmeistars.parser.CsvParser;
 import lv.progmeistars.parser.WordTranslationTypeData;
 
 import java.io.File;
+import java.util.Scanner;
 
 public class Main {
 
@@ -13,11 +13,26 @@ public class Main {
         var parser = new CsvParser();
 
         try {
-            var file = Main.class.getClassLoader().getResource("words.csv").getFile();
-            var list = parser.parseFile(new File(file));
 
-            for (WordTranslationTypeData data : list) {
-                System.out.println(data);
+            Scanner scanner = new Scanner(System.in);
+
+            if (scanner.nextInt() == 1) {
+                var file = Main.class.getClassLoader().getResource("words.csv").getFile();
+                var list = parser.parseFile(new File(file));
+
+                for (WordTranslationTypeData data : list) {
+                    System.out.println(data);
+                }
+            } else {
+
+                var scoreFile = Main.class.getClassLoader().getResource("score.csv").getFile();
+                var scoreList = parser.parseFile(new File(scoreFile));
+
+                System.out.println();
+
+                for (WordTranslationTypeData data : scoreList) {
+                    System.out.println(data);
+                }
             }
         } catch (NullPointerException e){
             System.out.println("y r wrong");
--- src/main/java/lv/progmeistars/parser/CsvParser.java
+++ src/main/java/lv/progmeistars/parser/CsvParser.java
@@ -1,7 +1,6 @@
 package lv.progmeistars.parser;
 
 import com.opencsv.CSVReader;
-import lombok.var;
 
 import java.io.File;
 import java.io.FileReader;
